5ede81810785ec1a9032b307fc479b9a4f826002
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2022-12-05 12:51:19 +0100

    updates

--- src/Functions.php
+++ src/Functions.php
@@ -1,5 +1,7 @@
 <?php
 namespace Catali;
+
+use stdClass;
 use TymFrontiers\InstanceError,
     TymFrontiers\Generic,
     TymFrontiers\Data,
@@ -7,6 +9,8 @@ use TymFrontiers\InstanceError,
     TymFrontiers\Validator,
     TymFrontiers\API,
     TymFrontiers\HTTP\Header;
+use TymFrontiers\MultiForm;
+use TymFrontiers\MySQLDatabase;
 use TymFrontiers\Session;
 
 function get_constant (string $name) {
@@ -163,5 +167,187 @@ function require_login (bool $redirect = true, string $rd_path = "/helper/user/l
     } else {
       Header::unauthorized(false,'',["Message"=>"Login is required for requested resource!"]);
     }
+  }
+}
+// Web Store functions
+function wsinfo (string $wsid = "", int $id_type = WSID_WSCODE):object|null {
+  if (empty($wsid)) {
+    $wsid = get_constant("PRJ_WSCODE");
+    $id_type = WSID_WSCODE;
+  } if (!\in_array($id_type,[WSID_WSCODE, WSID_DOMAIN, WSID_EMAIL])) {
+    throw new \Exception("Invalid ID type given in: \$param: 2", 1);
+  }
+  global $database;
+  global $color_theme;
+  global $session;
+  global $access_ranks;
+  $wsid = $database->escapeValue($wsid);
+  $data_db = \get_database("data");
+  $cnd = "";
+  switch ($id_type) {
+    case WSID_WSCODE:
+      $cnd = " AND ws.`code` = '{$wsid}' ";
+      break;
+    case WSID_DOMAIN:
+      $cnd = " AND ws.`domain` = '{$wsid}' ";
+      break;
+    case WSID_EMAIL:
+      $cnd = " AND ws.`email` = '{$wsid}' ";
+      break;
+    default:
+      $cnd = " AND ws.`code` = '{$wsid}' ";
+      break;
+  }
+  $wsobj = new MultiForm(\get_database("enterprise"), "ws", "code");
+  if ($found = $wsobj->findBySql("SELECT ws.code, ws.published, ws.status, ws.domain, ws.email, 
+                        ws.owner, ws.`type`, ws.category, ws.subcategory, ws.`name`, 
+                        ws.acronym, ws.description, ws.keywords, ws.brand_color, 
+                        ws._created AS created,
+                        tp.title AS type_title,
+                        ct.title AS category_title,
+                        sct.title AS subcategory_title
+                FROM :db:.:tbl: AS ws
+                LEFT JOIN `{$data_db}`.`business_types` AS tp ON tp.`name` = ws.`type`
+                LEFT JOIN `{$data_db}`.`business_category` AS ct ON ct.`name` = ws.category
+                LEFT JOIN `{$data_db}`.`business_subcategory` AS sct ON sct.`name` = ws.subcategory
+                WHERE ws.`status` NOT IN ('BANNED', 'SUSPENDED')
+                {$cnd}
+                LIMIT 1")
+  ) {
+    $found = $found[0];
+    return (object) [
+      "wscode" => $found->code,
+      "published" => (bool)$found->published,
+      "status" => $found->status,
+      "domain" => $found->domain,
+      "email" => $found->email,
+      "owner" => $found->owner,
+      "type" => (object) [
+        "name" => $found->type,
+        "title" => $found->type_title
+      ],
+      "category" => (object) [
+        "name" => $found->category,
+        "title" => $found->category_title
+      ],
+      "subcategory" => (object) [
+        "name" => $found->subcategory,
+        "title" => $found->subcategory_title
+      ],
+      "name" => $found->name,
+      "acronym" => $found->acronym,
+      "description" => $found->description,
+      "keywords" => \explode(",", $found->keywords),
+      "brand_color" => (object)[
+        "bg" => $color_theme[$found->brand_color]["hexcode"],
+        "fg" => $color_theme[$found->brand_color]["color"]
+      ],
+      "created" => $found->created
+    ];
+  } else {
+    // find what the error was
+    $err_output = [];
+    $wsobj->mergeErrors();
+    if ($errors = (new InstanceError($wsobj, ($session->access_rank() <= $access_ranks["ADMIN"])))->get("", true)) {
+      foreach ($errors as $method => $errs) {
+        foreach ($errs as $err) {
+          $err_output[] = "[{$method}]: " . $err;
+        }
+      }
+    } if (!empty($err_output)) {
+      throw new \Exception(\implode(PHP_EOL, $err_output), 1);
+    }
+  }
+  return null;
+}
+function wsowner (string $wsid = "", int $id_type = WSID_WSCODE):object|null {
+  if (empty($wsid)) {
+    $wsid = get_constant("PRJ_WSCODE");
+    $id_type = WSID_WSCODE;
+  } if (!\in_array($id_type,[WSID_WSCODE, WSID_DOMAIN, WSID_EMAIL])) {
+    throw new \Exception("Invalid ID type given in: \$param: 2", 1);
+  }
+  $db_user = \get_dbuser("CWS");
+  $db_server = \get_dbserver("CWS");
+  $db_name = \get_database("base", "CWS");
+  $data_db = \get_database("data", "CWS");
+  $ent_db = \get_database("enterprise", "CWS");
+  $conn = new MySQLDatabase($db_server, $db_user[0], $db_user[1], $db_name);
+  if (!$conn || !$conn instanceof MySQLDatabase) {
+    throw new \Exception("Server connection failed", 1);
+  }
+  $wsid = $conn->escapeValue($wsid);
+  $cnd = "";
+  switch ($id_type) {
+    case WSID_WSCODE:
+      $cnd = " AND `ws` = '{$wsid}' ";
+      break;
+    case WSID_DOMAIN:
+      $cnd = " AND `domain` = '{$wsid}' ";
+      break;
+    case WSID_EMAIL:
+      $cnd = " AND `email` = '{$wsid}' ";
+      break;
+    default:
+      $cnd = " AND `ws` = '{$wsid}' ";
+      break;
+  }
+
+  $owner = new MultiForm($db_name, "users", "code", $conn);
+  if ($user = $owner
+    ->findBySql("SELECT usr.`code`, usr.status, usr.`name`, usr.surname, usr.email, 
+                        usr.phone, usr.country_code,
+                        ct.name AS country
+                FROM :db:.:tbl: AS usr
+                LEFT JOIN `{$data_db}`.countries AS ct ON ct.`code` = usr.country_code
+                WHERE usr.`code` = (
+                  SELECT `owner`
+                  FROM `{$ent_db}`.ws_profile
+                  WHERE 1
+                  {$cnd}
+                  LIMIT 1
+                )
+                LIMIT 1")) {
+    $user = $user[0];
+    $conn->closeConnection();
+
+    return (object) [
+      "wscode" => $user->code,
+      "status" => $user->status,
+      "name" => $user->name,
+      "surname" => $user->surname,
+      "email" => $user->email,
+      "phone" => $user->phone,
+      "country" => (object) [
+        "code" => $user->country_code,
+        "name" => $user->country
+      ],
+    ];
+  } else {
+    // find what the error was
+    global $session;
+    global $access_ranks;
+    $err_output = [];
+    $owner->mergeErrors();
+    if ($errors = (new InstanceError($owner, ($session->access_rank() <= $access_ranks["ADMIN"])))->get("", true)) {
+      foreach ($errors as $method => $errs) {
+        foreach ($errs as $err) {
+          $err_output[] = "[{$method}]: " . $err;
+        }
+      }
+    } if (!empty($err_output)) {
+      throw new \Exception(\implode(PHP_EOL, $err_output), 1);
+    }
+  }
+  return null;
+}
+function checkws () {
+  $wsowner = wsowner();
+  if (!$wsowner || \in_array($wsowner->status, ["BANNED", "SUSPENDED", "DISABLED"])) {
+    Header::badRequest(true, "This web store cannot be viewed at this time. If you are the owner; kindly contact admin/support.");
+  }
+  $wsinfo = wsinfo();
+  if (!$wsinfo || \in_array($wsinfo->status, ["BANNED", "SUSPENDED", "DISABLED"])) {
+    Header::badRequest(true, "WS: This web store cannot be viewed at this time. If you are the owner; kindly contact admin/support.");
   }
 }
