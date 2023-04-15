fd0e74df5e10029c21404ab0b5cfd23947d78244
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2023-04-15 06:08:48 -0700

    update to functions

--- src/Functions.php
+++ src/Functions.php
@@ -35,6 +35,212 @@ function currency_decimals ($currency) {
   global $cryptos;
   return \array_key_exists($currency, $cryptos) ? 8 : 2;
 }
+function valid_coupon (string $code) {
+  global $database;
+  $ent_db = get_constant("MYSQL_ENTERPRISE_DB");
+  if (
+    $cpn = (new MultiForm(get_constant("MYSQL_BASE_DB"), "discount_coupons", "code"))
+      ->findBySql("SELECT cpn.`code`, cpn.name, cpn.payee_value, cpn.expiry, cpn.discount_type,
+                          cpn.discount_value, cpn.use_max,
+                          cpn.discount_cap, cpn.title, cpn.description, cpn.`store`, cpn.use_on,
+                          ws.name AS store_name, ws.weblink,
+                          (
+                            SELECT send
+                            FROM `{$ent_db}`.store_currency
+                            WHERE `store` = cpn.`store`
+                            LIMIT 1
+                          ) AS currency,
+                          (
+                            SELECT COUNT(*)
+                            FROM :db:.discount_coupon_usage
+                            WHERE `code` = cpn.`code`
+                          ) AS used
+                  FROM :db:.:tbl: AS cpn
+                  LEFT JOIN `{$ent_db}`.stores AS ws ON ws.`code` = cpn.`store`
+                  WHERE cpn.`code` = '{$database->escapeValue($code)}'
+                  OR cpn.`name` = '{$database->escapeValue($code)}'
+                  LIMIT 1")
+  ) {
+    $cpn = $cpn[0];
+    if (!empty($cpn->expiry) && \strtotime($cpn->expiry) <= \time()) {
+      throw new \Exception("Coupon expired", 1);
+    }
+    if ((int)$cpn->used >= (int)$cpn->use_max) {
+      throw new \Exception("Coupon has been used up!", 1);
+    }
+    $cpn_info = [
+      "code" => $cpn->code,
+      "name" => $cpn->name,
+      "used" => (int)$cpn->used,
+      "use_max" => (int)$cpn->use_max,
+      "currency" => $cpn->currency,
+      "payee_value" => $cpn->payee_value,
+      "expiry" => $cpn->expiry,
+      "title" => $cpn->title,
+      "description" => $cpn->description,
+      "store" => [
+          "name" => "{$cpn->store_name} (CWS " . code_split($cpn->store, " ") . ")",
+          "code" => $cpn->store,
+          "weblink" => $cpn->weblink
+        ],
+      "discount" => [
+        "type" => $cpn->discount_type,
+        "value" => (float)$cpn->discount_value,
+        "cap" => (float)$cpn->discount_cap,
+        "expiry" => empty($cpn->expiry) ? "NEVER" : (new BetaTym)->MDY($cpn->expiry)
+      ],
+      "applicable" => []
+    ];
+    $use_on = \explode(",",$cpn->use_on);
+    if (\count($use_on) < 1) {
+      throw new \Exception("Invalid coupon pattern.", 1);
+    }
+    foreach ($use_on as $key => $value) {
+      if (!empty($value)) {
+        $use_on[$key] = \trim($value);
+      } else {
+        unset($use_on[$key]);
+      }
+    }
+    if (\in_array("SERVICES",$use_on)) {
+      $cpn_info["applicable"]["type"] = "services";
+      $cpn_info["applicable"]["message"] = "Applies to all service from Web Store ({$cpn->store_name}) - {$cpn->weblink}";
+    } else if (\in_array("PRODUCTS",$use_on)) {
+      $cpn_info["applicable"]["type"] = "products";
+      $cpn_info["applicable"]["message"] = "Applies to all products from Web Store ({$cpn->store_name}) - {$cpn->weblink}";
+    } else if (\in_array("PACKAGES",$use_on)) {
+      $cpn_info["applicable"]["type"] = "packages";
+      $cpn_info["applicable"]["message"] = "Applies to all package from ({$cpn->store_name}) - {$cpn->weblink}";
+    } else {
+      $cpn_info["applicable"]["type"] = "selected";
+      $cpn_info["applicable"]["message"] = null;
+      $cpn_info["applicable"]["items"] = [];
+      $prd = [];
+      $svc = [];
+      $pkg = [];
+      foreach ($use_on as $itm) {
+        if (\strpos($itm, 'SVC.') !== false) {
+          $svc[] = $database->escapeValue(\str_replace('SVC.',"",$itm));
+        } else if (\strpos($itm, 'PRD.') !== false) {
+          $prd[] = $database->escapeValue(\str_replace('PRD.',"",$itm));
+        } else if (\strpos($itm, 'PKG.') !== false) {
+          $pkg[] = $database->escapeValue(\str_replace('PKG.',"",$itm));
+        } else {
+          throw new \Exception("Unknown coupon pattern.", 1);
+          break;
+        }
+      }
+      if (!empty($prd)) {
+        $prd = "'" . \implode("','",$prd) . "'";
+        if ($found = (new MultiForm(get_constant("MYSQL_ENTERPRISE_DB"), "store_products", "code"))->findBySql("SELECT `code`, title FROM :db:.:tbl: WHERE `code` IN ({$prd})")) {
+          $cpn_info["applicable"]["items"] = [];
+          $cpn_info["applicable"]["message"] = "Applies to following Products(s).";
+          foreach ($found as $itm) {
+            $cpn_info["applicable"]["items"][$itm->code] = $itm->title;
+          }
+        }
+      }
+      if (!empty($svc)) {
+        $svc = "'" . \implode("','",$svc) . "'";
+        if ($found = (new MultiForm(get_constant("MYSQL_ENTERPRISE_DB"), "store_services", "code"))->findBySql("SELECT `code`, title FROM :db:.:tbl: WHERE `code` IN ({$svc})")) {
+          $cpn_info["applicable"]["items"] = [];
+          $cpn_info["applicable"]["message"] = "Applies to following Service(s).";
+          foreach ($found as $itm) {
+            $cpn_info["applicable"]["items"][$itm->code] = $itm->title;
+          }
+        }
+      }
+      if (!empty($pkg)) {
+        $pkg = "'" . \implode("','",$pkg) . "'";
+        if ($found = (new MultiForm(get_constant("MYSQL_BASE_DB"), "packages", "code"))->findBySql("SELECT `code`, title FROM :db:.:tbl: WHERE `code` IN ({$pkg})")) {
+          $cpn_info["applicable"]["items"] = [];
+          $cpn_info["applicable"]["message"] = "Applies to following Package(s).";
+          foreach ($found as $itm) {
+            $cpn_info["applicable"]["items"][$itm->code] = $itm->title;
+          }
+        }
+      }
+    }
+    return $cpn_info;
+  }
+  return false;
+}
+function coupon (string $code, string $invoice) {
+  global $database;
+  $code = $database->escapeValue(\strtoupper(\str_replace(["-","_","."],"",$code)));
+  try {
+    $cpn = valid_coupon($code);
+  } catch (\Exception $e) {
+    throw new \Exception($e->getMessage(), 1);
+  }
+  if ($cpn == false) {
+    throw new \Exception("You have inputed an invalid coupon", 1);
+  }
+  $ent_db = get_constant("MYSQL_ENTERPRISE_DB");
+  $base_db = get_constant("MYSQL_BASE_DB");
+  // find matching items
+  $mq = "SELECT itm.type, itm.price, itm.code, ivc.total
+         FROM :db:.:tbl: AS itm
+         LEFT JOIN :db:.store_invoices AS ivc ON ivc.`code` = itm.invoice
+         WHERE itm.invoice = '{$database->escapeValue($invoice)}'";
+  if (!$items = (new MultiForm(get_constant("MYSQL_ENTERPRISE_DB"), "store_invoice_items", "id"))->findBySql($mq)) {
+    throw new \Exception("Applicable items not found for given invoice.", 1);
+  }
+  $itm_price = 0.00;
+  $ivc_total = (float)$items[0]->total;
+  $applicable = [];
+  foreach ($items as $itm) {
+    if (
+      (
+        $itm->type == "packages" && $cpn["applicable"]["type"] == "packages"
+        || $itm->type == "store_products" && $cpn["applicable"]["type"] == "products"
+        || $itm->type == "store_services" && $cpn["applicable"]["type"] == "services"
+      )
+      || \array_key_exists($itm->code, $cpn["applicable"]["items"])
+    ) {
+      $applicable[] = $itm->code;
+      $itm_price += (float)$itm->price;
+    }
+  }
+  if (empty($applicable)) {
+    throw new \Exception("There is nothing in your invoice that the coupon applies to.", 1);
+  }
+  if ($cpn["discount"]["type"] == "FIXED") {
+    $dsc = (float)$cpn["discount"]["value"];
+  } else {
+    $dsc = (float)$cpn["discount"]["value"] / 100 * $itm_price;
+    if ((float)$cpn["discount"]["cap"] > 0 && $dsc > (float)$cpn["discount"]["cap"] ) $dsc = (float)$cpn["discount"]["cap"];
+  }
+  if ($dsc > (float)$ivc_total ) $dsc = (float)$ivc_total;
+  return [
+    "code" => $cpn["code"],
+    "title" => $cpn["title"],
+    "discount" => $dsc,
+    "applicable_total" => $itm_price,
+    "new_price" => (float)$ivc_total - $dsc
+  ];
+}
+function coupon_summary (string $type, float $value, string|null $ext_type, int $ext_count, float $max_value = 0.00, string $currency = "USD") {
+  global $cryptos;
+  $return  = "";
+  if ($value > 0) {
+    $surfix = $type == "FLUID" ? "%" : "";
+    $prefix = $type == "FIXED" ? currency_symbol($currency) : "";
+    $value = $value <= 0 ? 0.00 : \number_format($value, (\in_array($currency, $cryptos) ? 8 : 2), ".", ",");
+    $max_value = $max_value <= 0 ? 0.00 : \number_format($max_value, (\in_array($currency, $cryptos) ? 8 : 2), ".", ",");
+    $return .= $prefix . $value . $surfix;
+
+    if ($max_value > 0) {
+  
+      $return .= (" (up to ". currency_symbol($currency). "{$max_value})");
+    }
+    $return .= " OFF";
+  } if (!empty($ext_type) && $ext_count > 0) {
+    $return .= " + additional {$ext_count} " .\ucwords(\strtolower($ext_type));
+    $return .= $ext_count > 1 ? "s" : "";
+  }
+  return $return;
+}
 function coupon_summary (string $type, float $value, string|null $ext_type, int $ext_count, float $max_value = 0.00, string $currency = "USD") {
   global $cryptos;
   $return  = "";
